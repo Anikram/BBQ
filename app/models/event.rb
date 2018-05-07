3ce5d985a065152c7825bc40c0660d29c8f10870
Anikram <4nikram@gmail.com>
2018-05-07 15:22:36 +0300

    events, users ready

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
 
+  belongs_to :user
+
+  validates :user, presence: true
   validates :title, presence: true, length: {maximum: 225}
   validates :address, presence: true
   validates :datetime, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 255}
+  validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
 end
