258be6142eca82f6629821035515ee63adb57af3
Anikram <4nikram@gmail.com>
2018-05-09 00:56:54 +0300

    less54hw1 - Sign in / User edit forms are updated. Form errors msg updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,27 @@ module ApplicationHelper
     asset_path('burger.png')
   end
 
+  # Anikram 2018 - Method declines word in russian
+  def decline_it (number, krokodil, krokodila, krokodilov)
+    if (number == nil || !number.is_a?(Numeric))
+      number = 0
+    end
+
+    remainder = number % 100
+
+    if (remainder >= 11 && remainder <= 20)
+      return krokodilov
+    end
+
+    remainder = number % 10
+
+    if (remainder == 1)
+      return  krokodil
+    elsif (remainder >= 2 && remainder <= 4)
+      return  krokodila
+    else
+      return krokodilov
+    end
+  end
+
 end
