96267d6e06be4a36a0150e11f7c1659811da9300
Anikram <4nikram@gmail.com>
2018-05-14 14:53:33 +0300

    lesson56+forms for subs

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,15 @@ class User < ActiveRecord::Base
 
   before_validation :set_name, pn: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "Товарисч #{rand(999)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: user.email).update_all(user_id: self.id)
   end
 end
