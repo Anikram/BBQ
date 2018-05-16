c67bd5dac758a79d92486254967e1a2ebcca6edf
Anikram <4nikram@gmail.com>
2018-05-16 11:30:39 +0300

    Lesson57-2 AmazonS3 prepare (2)

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,46 @@
+class PhotosController < ApplicationController
+  before_action :set_event, only: [:create, :destroy]
+  before_action :set_photo, only: [:destroy]
+
+
+  # POST /photos
+  def create
+    @new_photo = @event.photos.build(photo_params)
+    @new_photo.user = current_user
+    if @new_photo.save
+      redirect_to @event, notice: t('controllers.photo.created')
+    else
+      render 'events/show', alert: t('controllers.photo.errors.general')
+    end
+  end
+
+  # DELETE /photos/1
+  def destroy
+    message = { notice: t('controllers.photo.destroyed')}
+
+    if current_user_can_edit?(@photo)
+      @photo.destroy
+    else
+      message = {alert: t('controllers.photo.error')}
+    end
+
+    redirect_to @event, message
+  end
+
+  private
+  def set_photo
+    @photo = @event.photos.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+  # Only allow a trusted parameter "white list" through.
+  def photo_params
+    params.fetch(:photo, {}).permit(:photo, :user_id, :event_id)
+  end
+
+  def email_is_valid?
+    true unless User.all.map(&:email).select{ |x| x =~ @new_subscription.user_email}
+  end
+end
