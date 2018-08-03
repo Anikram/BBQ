c0e6a12dacaf955094b9042887cc18eba3904d25
Anikram <4nikram@gmail.com>
2018-08-03 12:24:49 +0300

    anonymus cant subscribe with used email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
 
-  before_create :user_is_owner?
+  before_create :user_is_owner? if -> {user.present?}
 
   validates :event, presence: true
   validates :user_name, presence: true, unless: 'user.present?'
@@ -11,6 +11,8 @@ class Subscription < ActiveRecord::Base
 
   validates :user_name, uniqueness: { scope: :event }
   validates :user_email, uniqueness: { scope: :event }
+
+  validate :email_uniqueness_for_anonymus, unless: -> {user.present?}
 
   def user_name
     if user.present?
@@ -29,6 +31,10 @@ class Subscription < ActiveRecord::Base
   end
 
   def user_is_owner?
-     throw :abort if self.event.user_id == self.user.id
+     errors.add(:user_email, :invalid) if self.event.user.email == user_email
+  end
+
+  def email_uniqueness_for_anonymus
+    errors.add(:user_email, :invalid) if User.exists?(email: self.user_email)
   end
 end
