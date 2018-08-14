cca64c9f8af194cef3e2325e5da09f941101e6d2
Anikram <4nikram@gmail.com>
2018-08-14 16:35:40 +0300

    carrierwave settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+=begin
   config.action_mailer.default_url_options = {host: 'thersty-beast.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
@@ -90,4 +90,5 @@ Rails.application.configure do
     :domain             => 'heroku.com',
     :enable_starttls_auto => true
   }
+=end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,8 @@ if Rails.env.production?
       provider:              'AWS',
       region:                'eu-central-1',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      use_iam_profile: true
 
     }
 
